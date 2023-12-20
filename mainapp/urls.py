e13e49235dad7a7018286fe801032041f1572d88
kartik <iamkaranmishra551@gmail.com>
2023-12-20 22:51:41 +0530

    Adding login route

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),    # Register User api
+    path('login/', views.login, name="login"),  # Login api
 ]
