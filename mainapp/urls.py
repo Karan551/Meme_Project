878f675301a7f0c8ae9940fce27a8d5ee98e5dbd
kartik <iamkaranmishra551@gmail.com>
2023-12-24 17:37:53 +0530

    Adding new memes template and required changes applied.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),    # Register User api
-    path('login/', views.login, name="login"),  # Login api
+    path('', views.index, name="index"),  # Register User api
+    path("login/", views.login, name="login"),  # Login api
+    path("memes/", views.get_memes, name="memes"),
+    path("logout/", views.logout, name="logout"),  # logout api
 ]
