717ceb2263d8c7af6566529f38cd280edce72110
kartik <iamkaranmishra551@gmail.com>
2023-12-24 17:38:59 +0530

    Session storage added .

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .utils import registerUser, loginUser
 import psycopg2
+# For session storage.
+from django.contrib.sessions.backends.db import SessionStore
+
 
 # from django.http import HttpResponse
 
@@ -20,6 +23,25 @@ try:
 except Exception as e:
     print("Error :", e)
     print("Database Connection failed")
+
+# ---------------For Session Handling--------------
+s = SessionStore()
+# print(s)
+
+
+# ---Middle ware---
+def checkSession():
+    """
+    brief: This function is used to check user's session is existed or not,
+            if exist then return True otherwise return False.
+    :return:bool
+    """
+    try:
+        email = s["email"]
+        return True
+    except Exception as error:
+        print("error:", error)
+        return False
 
 
 # Create your views here.
@@ -41,30 +63,70 @@ def index(request):
         response = registerUser(userDetails, cursor)
         # print("Total User is:", response["totalUsers"])
         if response["statusCode"] == 200:
-            return render(request, "index.html", {"msg": "Successfully Registered"})
+            s["email"] = userDetails["email"]
+            s["contact"] = userDetails["contact"]
+            print("This is registered user session:", s["email"])
+            print("This is session for register", s)
+            return redirect("/memes/")
+            # return render(request, "index.html", {"msg": "Successfully Registered"})
         else:
             return render(request, "index.html", {"msg": "Already Registered"})
     return render(request, "index.html")
 
 
 def login(request):
-    if request.method == "POST":
-        email = request.POST["email"]
-        password = request.POST["password"]
-        # print("User email is :", email)
-        # print("User password is :", password)
-        # collect user data to logged-in user.
-        userData = {
-            "email": email,
-            "password": password
-        }
-        # call the loginUser() function that is import from utils file.
-        response = loginUser(userData, cursor)
-        if response["statusCode"] == 200:
-            return render(request, "login.html", {"msg": response["message"]})
-        elif response["statusCode"] == 503 and response["message"] == "Password Incorrect.":
-            return render(request, "login.html", {"msg": response["message"]})
-        else:
-            return render(request, "login.html", {"msg": response["message"]})
+    sessionExist = checkSession()
+    # if session doesn't exist then login user and if user's session exist
+    # then show private page(meme page).
+    if not sessionExist:
+        if request.method == "POST":
+            email = request.POST["email"]
+            password = request.POST["password"]
+            # print("User email is :", email)
+            # print("User password is :", password)
+            # collect user data to logged-in user.
+            userData = {
+                "email": email,
+                "password": password
+            }
+            # call the loginUser() function that is import from utils file.
+            response = loginUser(userData, cursor)
+            if response["statusCode"] == 200:
+                # To Store Session
+                s["email"] = userData["email"]
+                s["password"] = userData["password"]
+                #  Then Redirect to the private route.
+                return redirect("/memes/")
+                # This is normally action occurs when user register.
+                # return render(request, "login.html", {"msg": response["message"]})
+            elif response["statusCode"] == 503 and response["message"] == "Password Incorrect.":
+                return render(request, "login.html", {"msg": response["message"]})
+            else:
+                return render(request, "login.html", {"msg": response["message"]})
 
-    return render(request, "login.html")
+        return render(request, "login.html")
+
+    # If session already exist then redirect to user memes/ route.
+    else:
+        return redirect("/memes/")
+
+
+def get_memes(request):
+    sessionExists = checkSession()
+    # if user's session doesn't exist the redirect to login user.
+    if not sessionExists:
+        redirect("/login/")
+    # if user's session exist then redirect to private page.
+    else:
+        return render(request, "memes.html")
+
+
+def logout(request):
+    # if user logged out successfully then redirect to the login page.
+    try:
+        s.clear()
+        return redirect("/login")
+    # if user already logged in then redirect to the private page.
+    except Exception as error:
+        print("This is error:", error)
+        return redirect("/memes/")
