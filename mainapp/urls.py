18e031714712234fb54a854b51a1c48a85a368f9
kartik <iamkaranmishra551@gmail.com>
2023-12-26 21:18:26 +0530

    adding edit_meme and details route.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("login/", views.login, name="login"),  # Login api
     path("memes/", views.get_memes, name="memes"),
     path("logout/", views.logout, name="logout"),  # logout api
+    path("edit_meme/", views.edit_memes, name="edit_meme"),  # edit meme
+    path("details/", views.meme_details, name="details"),
 ]
